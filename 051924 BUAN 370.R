11124659214d22dccd6b48b41018ea6d452347c1
StianK1 <167825049+StianK1@users.noreply.github.com>
2024-05-19 20:45:17 -0700

    Add files via upload

--- 051924 BUAN 370.R
+++ 051924 BUAN 370.R
@@ -3,4 +3,21 @@
 ############### Load Libaries ################
 library(ggplot2)
 
-############### Load Cleanred Customer Data Set ################
+#Import Data
+Customer <- read.csv(#instert cleaned data here#)
+
+View(Customer)
+
+#Check for NA Values
+sum(is.na(Customer))
+
+#Summary of the Data
+summary(Customer)
+
+# Identifying the class of each variable
+# Using sapply()
+sapply(Customer, class)
+
+####################### Cleaning ########################
+# Rename the Columns
+

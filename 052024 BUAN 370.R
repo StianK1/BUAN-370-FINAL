f33b2662cb8058dfa149eff838eef79ec2f46b6f
StianK1 <167825049+StianK1@users.noreply.github.com>
2024-05-20 11:30:12 -0700

    Add files via upload

--- 052024 BUAN 370.R
+++ 052024 BUAN 370.R
@@ -25,4 +25,37 @@ sapply(Spotify, class)
 
 ####################### Cleaning ########################
 # Rename the Columns
+colnames(Spotify)[colnames(Spotify)=="title"] <- "Song_Title"
+
+colnames(Spotify)[colnames(Spotify)=="artist"] <- "Artist_Name"
+
+colnames(Spotify)[colnames(Spotify)=="top genre"] <- "Top_Genre"
+
+colnames(Spotify)[colnames(Spotify)=="year released"] <- "Year_Released"
+
+colnames(Spotify)[colnames(Spotify)=="bpm"] <- "Beat_Per_Minute"
+
+colnames(Spotify)[colnames(Spotify)=="nrgy"] <- "Energy"
+
+colnames(Spotify)[colnames(Spotify)=="dnce"] <- "Danceability"
+
+colnames(Spotify)[colnames(Spotify)=="dB"] <- "Decibel_Meter"
+
+colnames(Spotify)[colnames(Spotify)=="live"] <- "Live"
+
+colnames(Spotify)[colnames(Spotify)=="val"] <- "Positivity"
+
+colnames(Spotify)[colnames(Spotify)=="dur"] <- "Duration"
+
+colnames(Spotify)[colnames(Spotify)=="acous"] <- "Acoustic"
+
+colnames(Spotify)[colnames(Spotify)=="spch"] <- "Speech"
+
+colnames(Spotify)[colnames(Spotify)=="pop"] <- "Popularity"
+
+colnames(Spotify)[colnames(Spotify)=="top year"] <- "Top_Year"
+
+colnames(Spotify)[colnames(Spotify)=="artist type"] <- "Artist_Type"
+
+
 
